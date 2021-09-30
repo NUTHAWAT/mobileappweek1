1e83f891260a9a93f358e55f07ce30635c170c62
NUTHAWAT <s6302041510141@email.kmutnb.ac.th>
2021-10-01 00:16:24 +0700

    WEEK12 CALL API

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobileappweek1/model/tct.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -8,10 +10,47 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  var data;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('hello');
+    callAPI();
+  }
+
+  Future<void> callAPI() async {
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+
+    var response = await http.get(url);
+
+    setState(() {
+      data = tctFromJson(response.body);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Dashboard'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Icon(Icons.api),
+            SizedBox(width: 10),
+            Text('Dashboard'),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Text(data?.activity ?? "Loading.."),
+            Text(data?.type ?? ""),
+            Text('${data?.price ?? ''}'),
+            Text('${data?.participants ?? ''}'),
+          ],
+        ),
+      ),
     );
   }
 }
